feefb69a6f860f1672ef333502db6126442e829c
Abbey-J <ajayn1@ocdsb.ca>
2018-03-28 10:39:48 -0400

    #3 Included

--- main.py
+++ main.py
@@ -18,13 +18,20 @@ def FtoC(F):
 answer = str(input("If you would like to convert temperature from Celsius to Fahrenheit, enter A. If you would like to convert temperature from Fahrenheit to Celsius, type B. "))
 
 if answer == "A":
-    C = int(input("Enter your temperature in degrees Celsius: "))
-    print("This temperature in degrees Fahrenheit is " + str(CtoF(C)))
+    C = float(input("Enter your temperature in degrees Celsius: "))
+    if C <= -273.15:
+        print("Physically impossible temperature. Restart and enter a warmer temperature")
+        exit()
+    print("This temperature in degrees Fahrenheit is " + int(CtoF(C)))
 elif answer == "B":
     F = int(input("Enter your temperature in degrees Fahrenheit: "))
+    if F <= -459.67:
+        print("Physically impossible temperature. Restart and enter a warmer temperature")
+        exit()
     print("This temperature in degrees Celsius is " + str(FtoC(F)))
 else:
     print("Invalid option. Choose either A or B.")
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    exit()
+
     
 
