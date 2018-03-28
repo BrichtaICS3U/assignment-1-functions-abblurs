ef53e6e423179345d8828b043d6bb44d1e46d50d
Abbey-J <ajayn1@ocdsb.ca>
2018-03-28 10:23:13 -0400

    Up to #3

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# Abbey Jayne
 # March 28, 2018
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF(C):
+    """Given a temperature in degrees Celcius, determine the
+    temperature in degrees Fahrenheit.
+    """
+    return round((1.8)*C + 32)
 
-###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC(F):
+    """Given a temperature in degrees Fahrenheit, determine the
+    temperature in degrees Celcius.
+    """
+    return round((0.55556)*(F-32))
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+answer = str(input("If you would like to convert temperature from Celsius to Fahrenheit, enter A. If you would like to convert temperature from Fahrenheit to Celsius, type B. "))
+
+if answer == "A":
+    C = int(input("Enter your temperature in degrees Celsius: "))
+    print("This temperature in degrees Fahrenheit is " + str(CtoF(C)))
+elif answer == "B":
+    F = int(input("Enter your temperature in degrees Fahrenheit: "))
+    print("This temperature in degrees Celsius is " + str(FtoC(F)))
+else:
+    print("Invalid option. Choose either A or B.")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    
+
